cf181f2c17b01682cda043f0423104a73e56cb22
Jose Antonio Perez <jose30a2@gmail.com>
2026-01-19 20:22:21 -0300

    Draw a card and get a value

--- assets/js/juego.js
+++ assets/js/juego.js
@@ -9,6 +9,7 @@ let deck = [];
 const tipos = ['C', 'D', 'H', 'S'];
 const especiales = ['A', 'J', 'Q', 'K'];
 
+// Crea una nueva baraja desordenada
 const createDeck = () => {
 
     for(let i=2; i<=10; i++){
@@ -23,12 +24,33 @@ const createDeck = () => {
         }
     }
 
-    console.log(deck);
-  
+    //console.log(deck);
+    // Desordena la baraja creada
     deck = _.shuffle(deck);
     
     console.log(deck);
-
 }
 
 createDeck();
+
+// Esta funcion permite coger una carta de la baraja
+const pedirCarta = () => {
+    if (deck.length === 0){
+        throw 'No hay cartas en el deck';
+    }
+    const carta = deck.pop();
+    console.log(carta);
+    console.log(deck);
+    return carta;
+}
+
+
+//pedirCarta();
+
+//Determina el valor de carta que se estrajo
+const valorCarta = (carta) => {
+    const valor = carta.substring(0, carta.length-1);
+    return  ( isNaN(valor)) ? ((valor==='A') ? 11 : 10 ) : valor*1; 
+}
+
+console.log(valorCarta('6D'));
