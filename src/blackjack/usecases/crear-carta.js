f94872edfeae7cf0b5c67e4c93f2983f0f7344a9
Jose Antonio Perez <jose30a2@gmail.com>
2026-01-26 11:01:38 -0300

    Correccion path cartas

--- src/blackjack/usecases/crear-carta.js
+++ src/blackjack/usecases/crear-carta.js
@@ -9,7 +9,7 @@
 export const crearCarta = (carta, turno, divCartasJugadores) => {
         const imgCarta = document.createElement('img');
         //imgCarta.src = `./assets/cartas/${ carta }.png`;
-        imgCarta.src = `./public/assets/cartas/${carta}.png`;
+        imgCarta.src = `./assets/cartas/${carta}.png`;
         imgCarta.classList.add('carta');
         divCartasJugadores[turno].append(imgCarta);
     }
